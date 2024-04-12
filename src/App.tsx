ae08eae991457c9a004e9928eb291e18cf3d8247
m.niestroj <mateusz.niestroj@equiqo.com>
2024-04-12 16:06:55 +0200

    Add Query Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { RouteObject } from 'react-router-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
@@ -18,6 +19,7 @@ export const App: FC<Props> = ({ createRouter = createBrowserRouter }) => {
     return (
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={createRouter(routes)} />
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     );
 };
